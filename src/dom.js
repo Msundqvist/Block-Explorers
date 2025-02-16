a77b7d7f4e043c3a3e31eac4131c883fac54f2a7
MatildaSundqvist <matildasundqvist@live.se>
2025-02-16 13:17:34 +0100

    refactor html to dom

--- src/dom.js
+++ src/dom.js
@@ -1,3 +1,6 @@
+import { formatEther } from 'https://esm.sh/viem';
+const transactionDetailDisplay = document.querySelector('#transactionDetails');
+
 export const createElement = (elem) => {
     return document.createElement(elem);
 };
@@ -5,4 +8,54 @@ export const createTextElement = (elem, text) => {
     const element = document.createElement(elem);
     element.appendChild(document.createTextNode(text));
     return element;
+};
+
+export const generateDisplay = (block, transaction) => {
+    let html = '';
+    transactionDetailDisplay.innerHTML = html;
+
+    html = `
+    <h2 id  ="blockNumber">Block Number: ${block.number}</h2>
+    <article class="trx-details">
+        <section>
+            <span>Gas Used</span>
+            <small>${block.gasUsed}</small>
+        </section>
+        <section>
+            <span>Gas limit</span>
+            <small>${block.gasLimit}</small>
+        </section>
+        <section>
+            <span>Mined on</span>
+            <small>${new Date(parseInt(block.timestamp * 1000n)).toLocaleString(
+        'sv-SE')}
+            </small>
+        </section>
+        <section>
+            <span>Block hash</span>
+            <small>${block.hash}</small>
+        </section>
+    </article>
+
+    <h2 id ="trxHash">Tx Hash ${transaction.hash}</h2>
+    <article class ="trx-details">
+        <section>
+            <span>From</span>
+            <small>${transaction.from}</small>
+        </section>
+        <section>
+            <span>To</span>
+            <small>${transaction.to}</small>
+        </section>
+        <section>
+            <span>Gas Used</span>
+            <small>${transaction.gas}</small>
+        </section>
+        <section>
+            <span>Value</span>
+            <small>${parseFloat(formatEther(transaction.value)).toFixed(2)}ETH</small>
+        </section >
+    </article > `;
+
+    transactionDetailDisplay.innerHTML = html;
 };
--- src/transaction.js
+++ src/transaction.js
@@ -1,6 +1,7 @@
-import { createPublicClient, http, formatEther } from 'https://esm.sh/viem';
+import { createPublicClient, http } from 'https://esm.sh/viem';
 import { localhost, sepolia } from 'https://esm.sh/viem/chains';
-const transactionDetailDisplay = document.querySelector('#transactionDetails');
+import { generateDisplay } from './dom.js';
+
 
 let client = undefined;
 
@@ -23,56 +24,6 @@ const displayTransactionDetails = async (hash) => {
         });
         generateDisplay(block, transaction);
     }
-};
-
-const generateDisplay = (block, transaction) => {
-    let html = '';
-    transactionDetailDisplay.innerHTML = html;
-
-    html = `
-    <h2 id  ="blockNumber">Block Number: ${block.number}</h2>
-    <article class="trx-details">
-        <section>
-            <span>Gas Used</span>
-            <small>${block.gasUsed}</small>
-        </section>
-        <section>
-            <span>Gas limit</span>
-            <small>${block.gasLimit}</small>
-        </section>
-        <section>
-            <span>Mined on</span>
-            <small>${new Date(parseInt(block.timestamp * 1000n)).toLocaleString(
-        'sv-SE')}
-            </small>
-        </section>
-        <section>
-            <span>Block hash</span>
-            <small>${block.hash}</small>
-        </section>
-    </article>
-
-    <h2 id ="trxHash">Tx Hash ${transaction.hash}</h2>
-    <article class ="trx-details">
-        <section>
-            <span>From</span>
-            <small>${transaction.from}</small>
-        </section>
-        <section>
-            <span>To</span>
-            <small>${transaction.to}</small>
-        </section>
-        <section>
-            <span>Gas Used</span>
-            <small>${transaction.gas}</small>
-        </section>
-        <section>
-            <span>Value</span>
-            <small>${parseFloat(formatEther(transaction.value)).toFixed(2)}ETH</small>
-        </section >
-    </article > `;
-
-    transactionDetailDisplay.innerHTML = html;
 };
 
 document.addEventListener('DOMContentLoaded', initApp);
